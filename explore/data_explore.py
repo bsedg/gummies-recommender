4180599d1bdb0641a02d5965c3e94e15e0efe122
Brandon Sedgwick <brandon@clipppr.com>
2014-05-19 22:58:50 -0400

    Add script to look at the data. Currently just attribute counts and scoring breakdown.

---
+++ explore/data_explore.py
@@ -0,0 +1,31 @@
+"""
+Using Pandas, look through the data and find
+any relevant statistics or correlations.
+"""
+import numpy as np
+import pandas as pd
+import json
+
+scores = [json.loads(line) for line in open('../data/cleaned_scores.json')]
+df = pd.DataFrame(scores)
+
+# counts for each column/key
+flavour_counts = df['flavour'].value_counts()
+size_counts = df['size'].value_counts()
+composition_counts = df['composition'].value_counts()
+colour_counts = df['colour-count'].value_counts()
+
+
+print "Flavour: \n\tsour: %d\n\tsweet: %d" % \
+    (flavour_counts['sour'], flavour_counts['sweet'])
+print "Size: \n\tsmall: %d\n\tmedium: %d\n\tlarge: %d" % \
+    (size_counts['small'], size_counts['medium'], size_counts['large'])
+print "Composition: \n\tgummy: %d\n\tmarshmallow: %d\n\tjube-jube: %d" % \
+    (composition_counts['gummy'],
+        composition_counts['marshmallow'],
+        composition_counts['jube-jube'])
+print "Colour Count: \n\t1: %d\n\t2: %d\n\t4: %d" % \
+    (colour_counts[1], colour_counts[2], colour_counts[4])
+
+print "Score breakdown:"
+print df.describe()['score']
